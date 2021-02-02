76d74e2e4f027b1fe3b64aee3517c541244bf7a0
Kristy Ou <ou.kristy@gmail.com>
2021-02-02 14:59:16 +0100

    modified auc

--- ATAC/atac_prepro/genrich.sh
+++ ATAC/atac_prepro/genrich.sh
@@ -5,12 +5,13 @@
 #SBATCH --mem=60G
 #SBATCH --time=20:00:00
 #SBATCH --job-name="atac_genrich"
-#SBATCH --output=./logs/reproPeaks_atac_genrich.stdout
-#SBATCH --error=./logs/reproPeaks_atac_genrich.stderr
+#SBATCH --output=./logs/reproPeaks2_atac_genrich.stdout
+#SBATCH --error=./logs/reproPeaks2_atac_genrich.stderr
 #SBATCH --export=ALL
 
 
 # This script takes the ATAC-seq bam files (sorted by readnames in samtools), runs Genrich to call peaks, and ouputs a narrowpeak file
+# run from ~/msc/ATAC w/ sbatch 
 
 # Change directory
 cd ~/work/msc/ATAC/peaks 
@@ -32,21 +33,21 @@ hg19="../../deeptools/hg19.blacklist.bed"
 # get comma separated list of BM bam files
 BM=`echo ${bamdir}/BM* | tr ' ' ,`
 
-Genrich -t $BM \
-  -o reproPeaks/BM_ATAC.narrowPeak \
-  -e chrY -E $hg19 \
-  -j -q 0.05 -a 20 -v
-
+#Genrich -t $BM \
+#  -o reproPeaks/BM_ATAC.narrowPeak \
+#  -e chrY -E $hg19 \
+#  -j -q 0.05 -a 20 -v
+#
 ## == CH == ##
 CH=`echo ${bamdir}/CH* | tr ' ' ,`
 
 Genrich -t $CH \
   -o reproPeaks/CH_ATAC.narrowPeak \
   -e chrY -E $hg19 \
-  -j -q 0.05 -a 5 -v 
+  -j -q 0.05 -a 1 -v 
 
 ## == FB == ##
-#FB=`echo ${bamdir}/FB* | tr ' ' ,`
+FB=`echo ${bamdir}/FB* | tr ' ' ,`
 #
 #Genrich -t $FB \
 #  -o reproPeaks/FB_ATAC.narrowPeak \
@@ -59,10 +60,10 @@ UC=`echo ${bamdir}/UC* | tr ' ' ,`
 Genrich -t $UC \
   -o reproPeaks/UC_ATAC.narrowPeak \
   -e chrY -E $hg19 \
-  -j -q 0.05 -a 5 -v
+  -j -q 0.05 -a 1 -v
 
 ## == WAT == ##
-#WAT=`echo ${bamdir}/WAT* | tr ' ' ,`
+WAT=`echo ${bamdir}/WAT* | tr ' ' ,`
 #
 #Genrich -t $WAT \
 #  -o reproPeaks/WAT_ATAC.narrowPeak \
