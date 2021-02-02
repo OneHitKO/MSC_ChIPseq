fdf1e5f4b980dd3e7c8592639c218833337a81d0
Kristy Ou <ou.kristy@gmail.com>
2021-02-02 13:40:00 +0100

    fixed output

--- ATAC/atac_prepro/bedtools_unique_K27ac.sh
+++ ATAC/atac_prepro/bedtools_unique_K27ac.sh
@@ -11,20 +11,15 @@ in="./peaks/reproPeaks"
 out="./peaks/uniquePeaks"
 
 # first find strictly unique peaks
-bedtools intersect -a ${in}/BM* -b ${in}/UC* ${in}/WAT* ${in}/FB* ${in}/CH* -v
-  > ${out}/uniqueBM.narrowPeak 
+bedtools intersect -a ${in}/BM* -b ${in}/UC* ${in}/WAT* ${in}/FB* ${in}/CH* -v > ${out}/uniqueBM.narrowPeak 
 
-bedtools intersect -a ${in}/CH* -b ${in}/UC* ${in}/WAT* ${in}/FB* ${in}/BM* -v
-  > ${out}/uniqueCH.narrowPeak
+bedtools intersect -a ${in}/CH* -b ${in}/UC* ${in}/WAT* ${in}/FB* ${in}/BM* -v > ${out}/uniqueCH.narrowPeak
 
-bedtools intersect -a ${in}/FB* -b ${in}/BM* ${in}/CH* ${in}/UC* ${in}/WAT* -v
-  > ${out}/uniqueFB.narrowPeak 
+bedtools intersect -a ${in}/FB* -b ${in}/BM* ${in}/CH* ${in}/UC* ${in}/WAT* -v > ${out}/uniqueFB.narrowPeak 
 
-bedtools intersect -a ${in}/UC* -b ${in}/BM* ${in}/CH* ${in}/FB* ${in}/WAT* -v
-  > ${out}/uniqueUC.narrowPeak
+bedtools intersect -a ${in}/UC* -b ${in}/BM* ${in}/CH* ${in}/FB* ${in}/WAT* -v > ${out}/uniqueUC.narrowPeak
 
-bedtools intersect -a ${in}/WAT* -b ${in}/BM* ${in}/CH* ${in}/FB* ${in}/UC* -v
-  > ${out}/uniqueWAT.narrowPeak
+bedtools intersect -a ${in}/WAT* -b ${in}/BM* ${in}/CH* ${in}/FB* ${in}/UC* -v > ${out}/uniqueWAT.narrowPeak
 
 
 
