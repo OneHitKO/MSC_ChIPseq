9a7441411cf9ebcde8e8110ff354d9586d4e31c6
Kristy Ou <ou.kristy@gmail.com>
2021-02-15 22:16:55 +0100

    bedtools for unique ATAC peaks

--- ATAC/atac_prepro/bedtools_homer_uniqueATAC.sh
+++ ATAC/atac_prepro/bedtools_homer_uniqueATAC.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Script to get unique regions to run on homer
+# Script to identify enriched motifs on unique ATAC-seq Peaks (they have not
+# been stratified by H3K27ac)
 # Run script on computing node with ng conda environment
 
 # cd to directory containing reproducible unique ATAC peaks
@@ -11,15 +12,15 @@ in="./peaks/reproPeaks"
 out="./peaks/uniquePeaks"
 
 # first find strictly unique peaks
-#bedtools intersect -a ${in}/BM* -b ${in}/UC* ${in}/WAT* ${in}/FB* ${in}/CH* -v > ${out}/uniqueBM.narrowPeak 
+bedtools intersect -a ${in}/BM* -b ${in}/UC* ${in}/WAT* ${in}/FB* ${in}/CH* -v > ${out}/uniqueBM.narrowPeak 
 
-#bedtools intersect -a ${in}/CH* -b ${in}/UC* ${in}/WAT* ${in}/FB* ${in}/BM* -v > ${out}/uniqueCH.narrowPeak
+bedtools intersect -a ${in}/CH* -b ${in}/UC* ${in}/WAT* ${in}/FB* ${in}/BM* -v > ${out}/uniqueCH.narrowPeak
 
-#bedtools intersect -a ${in}/FB* -b ${in}/BM* ${in}/CH* ${in}/UC* ${in}/WAT* -v > ${out}/uniqueFB.narrowPeak 
+bedtools intersect -a ${in}/FB* -b ${in}/BM* ${in}/CH* ${in}/UC* ${in}/WAT* -v > ${out}/uniqueFB.narrowPeak 
 
-#bedtools intersect -a ${in}/UC* -b ${in}/BM* ${in}/CH* ${in}/FB* ${in}/WAT* -v > ${out}/uniqueUC.narrowPeak
+bedtools intersect -a ${in}/UC* -b ${in}/BM* ${in}/CH* ${in}/FB* ${in}/WAT* -v > ${out}/uniqueUC.narrowPeak
 
-#bedtools intersect -a ${in}/WAT* -b ${in}/BM* ${in}/CH* ${in}/FB* ${in}/UC* -v > ${out}/uniqueWAT.narrowPeak
+bedtools intersect -a ${in}/WAT* -b ${in}/BM* ${in}/CH* ${in}/FB* ${in}/UC* -v > ${out}/uniqueWAT.narrowPeak
 
 
 # motif enrichment analysis on unique ATAC seq peaks
