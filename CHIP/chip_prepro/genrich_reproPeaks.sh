acb51f1d8cdab72de84eb31d7b4d94258a2ac42b
Kristy Ou <ou.kristy@gmail.com>
2021-04-06 14:02:14 +0200

    updated repro peaks

--- CHIP/chip_prepro/genrich_reproPeaks.sh
+++ CHIP/chip_prepro/genrich_reproPeaks.sh
@@ -7,128 +7,57 @@
 #
 # chrY, chrM, and hg19 blacklisted regions are filtered out. 
 
-module load gcc
-conda activate ng 
+cd ~/work/msc/CHIP/
 
-mkdir -p ./reproPeaks
+#module load gcc
+#conda init bash
+#conda activate ng 
 
-out="./reproPeaks"
+# input dir with resorted bam files
+in="./resorted/"
 
-### Create comma separated lists for Genrich input!
+# create output dir
+mkdir -p ./peaks/reproPeaks
+out="./peaks/reproPeaks"
 
-## define H3K4me3 ##
-K4m3BM=`echo resorted/K4m3-BM* | tr ' ' ,`
-K4m3CH=`echo resorted/K4m3-CH* | tr ' ' ,`
-K4m3FB=`echo resorted/K4m3-FB* | tr ' ' ,`
-K4m3UC=`echo resorted/K4m3-UC* | tr ' ' ,`
-K4m3WAT=`echo resorted/K4m3-WAT* | tr ' ' ,`
-
-## define H3K27ac ##
-K27acBM=`echo resorted/K27ac-BM* | tr ' ' ,`
-K27acCH=`echo resorted/K27ac-CH* | tr ' ' ,`
-K27acFB=`echo resorted/K27ac-FB* | tr ' ' ,`
-K27acUC=`echo resorted/K27ac-UC* | tr ' ' ,`
-K27acWAT=`echo resorted/K27ac-WAT* | tr ' ' ,`
-
-## define H3K4me1 ## 
-K4m1BM=`echo resorted/K4m1-BM* | tr ' ' ,`
-K4m1CH=`echo resorted/K4m1-CH* | tr ' ' ,`
-K4m1FB=`echo resorted/K4m1-FB* | tr ' ' ,`
-K4m1UC=`echo resorted/K4m1-UC* | tr ' ' ,`
-K4m1WAT=`echo resorted/K4m1-WAT* | tr ' ' ,`
-
-## define input ##
-inBM=`echo resorted/input-BM* | tr ' ' ,`
-inCH=`echo resorted/input-CH* | tr ' ' ,`
-inFB=`echo resorted/input-FB* | tr ' ' ,`
-inUC=`echo resorted/input-UC* | tr ' ' ,`
-inWAT=`echo resorted/input-WAT* | tr ' ' ,`
-
+# other param for Genrich
 hg19="../deeptools/hg19.blacklist.bed"
 chr="chrY,chrM"
 
-# to do: make more elegant w/ loops
-
-## Call BM peaks
-Genrich -t $K4m3BM -c $inBM \
-  -o ${out}/BM_K4m3.bed \
-  -e $chr -E $hg19 -q 0.05 -v -y -m 30 -a 100.0
-
-Genrich -t $K27acBM -c $inBM \
-  -o ${out}/BM_K27ac.bed \
-  -e $chr -E $hg19 -q 0.05 -v -y -m 30 -a 100.0
-
-Genrich -t $K4m1BM -c $inBM \
-  -o ${out}/BM_K4m1.bed \
-  -e $chr -E $hg19 -q 0.05 -v -y -m 30 -a 20.0
-
-## Call CH peaks 
-Genrich -t $K4m3CH -c $inCH \
-  -o ${out}/CH_K4m3.bed \
-  -e $chr -E $hg19 -q 0.05 -v -y -m 30 -a 100.0
-
-Genrich -t $K27acCH -c $inCH \
-  -o ${out}/CH_K27ac.bed \
-  -e $chr -E $hg19 -q 0.05 -v -y -m 30 -a 100.0
-
-Genrich -t $K4m1CH -c $inCH \
-  -o ${out}/CH_K4m1.bed \
-  -e $chr -E $hg19 -q 0.05 -v -y -m 30 -a 20.0
-
-## Call FB peaks
-Genrich -t $K4m3FB -c $inFB \
-  -o ${out}/FB_K4m3.bed \
-  -e $chr -E $hg19 -q 0.05 -v -y -m 30 -a 100.0
-
-Genrich -t $K27acFB -c $inFB \
-  -o ${out}/FB_K27ac.bed \
-  -e $chr -E $hg19 -q 0.05 -v -y -m 30 -a 100.0
-
-Genrich -t $K4m1FB -c $inFB \
-  -o ${out}/FB_K4m1.bed \
-  -e $chr -E $hg19 -q 0.05 -v -y -m 30 -a 20.0
-
-## Call UC peaks 
-Genrich -t $K4m3UC -c $inUC \
-  -o ${out}/UC_K4m3.bed \
-  -e $chr -E $hg19 -q 0.05 -v -y -m 30 -a 100.0
-
-Genrich -t $K27acUC -c $inUC \
-  -o ${out}/UC_K27ac.bed \
-  -e $chr -E $hg19 -q 0.05 -v -y -m 30 -a 100.0
-
-Genrich -t $K4m1UC -c $inUC \
-  -o ${out}/UC_K4m1.bed \
-  -e $chr -E $hg19 -q 0.05 -v -y -m 30 -a 20.0
-
-## Call WAT peaks
-Genrich -t $K4m3WAT -c $inWAT \
-  -o ${out}/WAT_K4m3.bed \
-  -e $chr -E $hg19 -q 0.05 -v -y -m 30 -a 100.0
-
-Genrich -t $K27acWAT -c $inWAT \
-  -o ${out}/WAT_K27ac.bed \
-  -e $chr -E $hg19 -q 0.05 -v -y -m 30 -a 100.0
-
-Genrich -t $K4m1WAT -c $inWAT \
-  -o ${out}/WAT_K4m1.bed \
-  -e $chr -E $hg19 -q 0.05 -v -y -m 30 -a 20.0
+# create tissue array
+tissue=("BM" "CH" "FB" "UC" "WAT")
 
 
+# loop through each tissue
+for i in "${tissue[@]}"
+do
+  echo $i
 
+  # fine resorted bam files matching tissue pattern
+  k27ac=`echo $(find ${in}/K27ac*.bam -type f -print | grep "\-${i}\-")`
+  k4m3=`echo $(find ${in}/K4m3*.bam -type f -print | grep "\-${i}\-")`
+  k4m1=`echo $(find ${in}/K4m1*.bam -type f -print | grep "\-${i}\-")`
 
+  
+  # perform peak calling separately (might need to adjust param)
+  # chip-seq do NOT have PE reads! use -y argument!
+  echo "$k27ac"
+  Genrich -t "${k27ac}" -c "${k27ac//K27ac/input}" \
+    -o ${out}/${i}_k27ac.narrowPeak \
+    -e $chr -E $hg19 -y -m 30 \
+    -q 0.05 -a 100.0
 
+  echo "$k4m3"
+  Genrich -t "${k4m3}" -c "${k4m3//K4m3/input}" \
+    -o ${out}/${i}_k4m3.narrowPeak \
+    -e $chr -E $hg19 -y -m 30 \
+    -q 0.05 -a 100.0
 
+  echo "$k4m1"
+  Genrich -t "${k4m1}" -c "${k4m1//K4m1/input}" \
+    -o ${out}/${i}_k4m1.narrowPeak \
+    -e $chr -E $hg19 -y -m 30 \
+    -q 0.1 -a 50.0 -g 400
 
-
-
-
-
-
-
-
-
-
-
-
+done
 
